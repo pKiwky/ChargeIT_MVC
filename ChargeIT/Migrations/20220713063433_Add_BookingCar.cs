14d7e8ab638a3ab5924c336d5139173b0c03884c
Alex <Alex@DESKTOP-38MKITO>
2022-07-13 15:40:54 +0300

    Migrations

---
+++ ChargeIT/Migrations/20220713063433_Add_BookingCar.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ChargeIT.Migrations
+{
+    public partial class Add_BookingCar : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CarId",
+                table: "Bookings",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bookings_CarId",
+                table: "Bookings",
+                column: "CarId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Bookings_Cars_CarId",
+                table: "Bookings",
+                column: "CarId",
+                principalTable: "Cars",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Bookings_Cars_CarId",
+                table: "Bookings");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Bookings_CarId",
+                table: "Bookings");
+
+            migrationBuilder.DropColumn(
+                name: "CarId",
+                table: "Bookings");
+        }
+    }
+}
